349923a10eb2eb6397789b2625f24aec7815bf1a
Iva Havranová <havranova.i@seznam.cz>
2023-05-28 16:17:03 +0200

    Iva and AboutProject component done

--- src/sections/AboutProject/aboutproject.jsx
+++ src/sections/AboutProject/aboutproject.jsx
@@ -2,5 +2,25 @@ import React from 'react';
 import './aboutproject.css';
 
 export const AboutProject = () => {
-  return <section id="aboutproject">O projektu</section>;
+  return (
+    <>
+      <section id="aboutproject">
+        <h1>O projektu</h1>
+        <p>
+          Pokud máte rádi pěší turistiku a cestování, určitě vás budou zajímat
+          informace o nejkrásnějších a nejzajímavějších dálkových trasách, které
+          na světě existují. Tyto trasy nabízejí nejen možnost poznat krásy
+          přírody, ale také vás provedou historickými a kulturními památkami. Na
+          naší stránce se zaměřujeme na představení dálkových tras, které nás
+          zaujaly, nebo které jsme sami absolvovali. Naším cílem je poskytnout
+          vám inspiraci pro vaše pěší výlety a pomoci vám naplánovat vaši
+          vlastní dálkovou túru. Na této stránce se dozvíte více o jednotlivých
+          trasách, včetně délky, obtížnosti, zajímavostí, které po cestě můžete
+          vidět, a dalších užitečných informacích pro vaši cestu. Přejeme vám
+          příjemné prohlížení našich stránek a mnoho nezapomenutelných zážitků
+          na vašich budoucích túrách!
+        </p>
+      </section>
+    </>
+  );
 };
--- src/sections/Iva/index.jsx
+++ src/sections/Iva/index.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './style.css';
+import imgIva from './img/Bety.jpg';
 
 export const Iva = () => {
-  return <section id="iva">Iva alalalalala</section>;
+  return (
+    <section id="iva">
+      <div className="intro__iva">
+        <div className="intro__text">
+          Ahoj, já jsem Iva a na fotce je se mnou naše psí láska Bety. Kromě
+          učení se nových věcí je mojí vášní šití a hrozně se těším na všechny
+          projekty, které v budoucnu nakódím a naprogramuji. V příštích dnech to
+          bude projekt Hike it and like it, na kterým pracuju spolu s Míšou a
+          našimi mentory Kájou a Sergejem. A co mi nejde? Psát něco o sobě, tak
+          raději končím :D.
+        </div>
+        <div className="intro__photo">
+          <img
+            className="intro__photo--iva"
+            src={imgIva}
+            alt="Fotka Ivy"
+            width="300"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
