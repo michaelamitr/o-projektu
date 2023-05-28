545fae6d590f5ce38d0ea7e4c8e650824a23d21d
Michaela Trčková <misa.trckova@volny.cz>
2023-05-28 15:04:36 +0200

    misa section added, footer css

--- src/sections/Misa/misa.jsx
+++ src/sections/Misa/misa.jsx
@@ -2,5 +2,20 @@ import React from 'react';
 import './misa.css';
 
 export const Misa = () => {
-  return <section id="misa">Místo pro Míšu!</section>;
+  return (
+    <section id="misa">
+      <div className="description">
+        <p>Ahoj!</p>
+        <p>
+          Jmenuji se Michaela Trčková a společně s mou drahou dvojičkou – Ivou
+          Havranovou zpracovávám projekt Hike it and like it.
+        </p>
+        <p>
+          Moc ráda cestuji, poznávám nové kultury, učím se cizí jazyky a
+          poznávám krásy přírody. Jelikož Iva je na stejné vlně jako já, shodly
+          jsme se, že web o různých světových trasách bude to pravé pro nás.
+        </p>
+      </div>
+    </section>
+  );
 };
